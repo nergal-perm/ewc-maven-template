ab9dbd43a5fec9723a8e3038f6448deaee8e158d
Евгений Терехов <nergal@psu.ru>
2025-08-21 13:14:31 +0400

    feat: basic pom and sample source code

---
+++ src/test/java/ewc/SomeTest.java
@@ -0,0 +1,11 @@
+package ewc;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SomeTest {
+    @Test
+    void abstractTest() {
+        assertThat("Hello, world!").isNotBlank();
+    }
+}
